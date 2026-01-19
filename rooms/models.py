0dae79d7aee14034ea71be35b0db477bf8894160
FathimaHananKA <kafathimahanan@gmail.com>
2026-01-19 11:17:58 +0530

    details

--- rooms/models.py
+++ rooms/models.py
@@ -8,6 +8,15 @@ class Room(models.Model):
         on_delete=models.CASCADE,
         related_name='rooms'
     )
+    floor = models.PositiveIntegerField(default=1)
+    ROOM_TYPES = (
+        ('Single', 'Single'),
+        ('Double', 'Double'),
+        ('Triple', 'Triple'),
+        ('Quad', 'Quad'),
+    )
+    room_type = models.CharField(max_length=20, choices=ROOM_TYPES, default='Double')
+    total_beds = models.PositiveIntegerField(default=2)
 
     def __str__(self):
         return f"{self.room_number} - {self.dormitory.name}"
